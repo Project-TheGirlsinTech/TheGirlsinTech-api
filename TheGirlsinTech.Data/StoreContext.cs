e7c2dab9e80753215060c9b040dcdb59d1e8b012
NinaNikazm <nikazm.nina@gmail.com>
2023-11-16 18:39:20 -0500

    4. Adding Startup Data

--- TheGirlsinTech.Data/StoreContext.cs
+++ TheGirlsinTech.Data/StoreContext.cs
@@ -9,6 +9,12 @@ namespace TheGirlsinTech.Data
         : base(options)
         { }
         public DbSet<Item> Items { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            DbInitializer.Initialize(builder);
+        }
     }
 }
 
