ee0ffcd79f6a61fc21562c8f7862046a53190beb
NinaNikazm <nikazm.nina@gmail.com>
2023-11-15 22:44:57 -0500

    part 2

---
+++ TheGirlsinTech.Api/Controllers/CatalogController.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+using TheGirlsinTech.Domain.Catalog;
+
+namespace TheGirlsinTech.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CatalogController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetItems()
+        {
+            var items = new List<Item>()
+            {
+                new Item("Shirt", "Ohio State shirt.", "Nike", 29.99m),
+                new Item("Shorts", "Ohio State shorts.", "Nike", 44.99m)
+            };
+
+            return Ok(items);
+        }
+    }
+}
