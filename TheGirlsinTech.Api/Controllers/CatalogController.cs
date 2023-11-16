46c8441983c48f9b84063cb4a4c34c4f59189885
NinaNikazm <nikazm.nina@gmail.com>
2023-11-16 18:52:12 -0500

    5. Adding Post

--- TheGirlsinTech.Api/Controllers/CatalogController.cs
+++ TheGirlsinTech.Api/Controllers/CatalogController.cs
@@ -41,7 +41,9 @@ namespace TheGirlsinTech.Api.Controllers
         [HttpPost]
         public IActionResult Post(Item item)
         {
-            return Created("/catalog/42", item);
+            _db.Items.Add(item);
+            _db.SaveChanges();
+            return Created($"/catalog/{item.Id}", item);
         }
 
         [HttpPost("{id:int}/ratings")]
