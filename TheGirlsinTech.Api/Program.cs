c10d54d64be728753188abd77d1939934584d852
NinaNikazm <nikazm.nina@gmail.com>
2023-12-05 18:09:09 -0500

    3. Fixing react key binding issue

--- TheGirlsinTech.Api/Program.cs
+++ TheGirlsinTech.Api/Program.cs
@@ -12,6 +12,17 @@ builder.Services.AddDbContext<StoreContext>(options =>
     options.UseSqlite("Data Source=../Registrar.sqlite",
     b => b.MigrationsAssembly("TheGirlsinTech.Api"))
     );
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(builder =>
+    {
+        builder.WithOrigins("http://localhost:3000")
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -26,6 +37,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors();
 
 app.UseAuthorization();
 
